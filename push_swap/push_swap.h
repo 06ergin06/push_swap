26e9760f4795e52d2fb267224271ffd236020b1d
İbrahim Hakkı Ergin <72016401+06ergin06@users.noreply.github.com>
2026-03-01 17:06:09 +0300

    Add files via upload

---
+++ push_swap/push_swap.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iergin <iergin@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/01 15:28:17 by iergin            #+#    #+#             */
+/*   Updated: 2026/03/01 17:02:46 by iergin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_node
+{
+	int				content;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_list;
+
+void	append_node(t_list **stack, int value);
+
+void	sa(t_list **stack_a);
+void	sb(t_list **stack_b);
+void	ss(t_list **stack_a,t_list **stack_b);
+void	pa(t_list **stack_a, t_list **stack_b);
+void	pb(t_list **stack_a, t_list **stack_b);
+
+
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+
+int	ft_atoi(const char *str);
+#endif
